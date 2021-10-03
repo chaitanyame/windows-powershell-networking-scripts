52938d1c32a0b8f0b062b586d34359c8dc43da59
Chaitanya Talasila <chaitanya_talasila@outlook.com>
2021-10-03 15:37:24 +0530

    intial commit

---
+++ send_UDP_Packets.ps1
@@ -0,0 +1,27 @@
+﻿
+function Send-UdpDatagram
+{
+      Param ([string] $EndPoint, 
+      [int] $Port, 
+      [string] $Message)
+
+      $IP = [System.Net.Dns]::GetHostAddresses($EndPoint) 
+      $Address = [System.Net.IPAddress]::Parse($IP) 
+      $EndPoints = New-Object System.Net.IPEndPoint($Address, $Port) 
+      $Socket = New-Object System.Net.Sockets.UDPClient 
+      $EncodedText = [Text.Encoding]::ASCII.GetBytes($Message) 
+      $SendMessage = $Socket.Send($EncodedText, $EncodedText.Length, $EndPoints) 
+      $Socket.Close() 
+} 
+
+
+$ipaddress='10.71.7.180'
+$portnumber=1813
+
+for ($count=1 ; $count -le 1000000 ; $count++)
+{
+
+Send-UdpDatagram -EndPoint ipaddress -Port $portnumber -Message " Test Message= {$($count)}" 
+
+
+}
